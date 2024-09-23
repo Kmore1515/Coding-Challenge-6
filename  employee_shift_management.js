38d90c3a861c567d9179baa09a9a85519afb3a7e
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-22 20:28:55 -0400

    Create displayEmployeeShifts function

---  employee_shift_management.js
+++  employee_shift_management.js
@@ -6,3 +6,16 @@ const employees = [
     { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
 ]
+// Task 2: Create a Function to Display Employee Shift Details
+function displayEmployeeShifts(employeeName) {
+    let employee = employees.find(employee => employee.name === employeeName) // Finding the employee in the array.
+    if (!employee) {
+        return `${employeeName} is not in database.`} // Incase a non-employee gets entered
+        console.log(`${employeeName} has the following shifts`)
+    return employee.shifts.map(shift =>{
+        return `${shift.hours} hours on ${shift.day}` // This displays the shifts given.
+        });
+    
+}
+console.log(displayEmployeeShifts("John"));
+
