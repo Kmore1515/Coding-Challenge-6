95c716c6dbee02543df91e74e87a7fccf960f6c4
Kmore1515 <kylebradleymorris@usf.edu>
2024-09-22 22:04:10 -0400

    Create displayEmployeeShifts function

---  employee_shift_management.js
+++  employee_shift_management.js
@@ -5,17 +5,16 @@ const employees = [
     { name: 'Sara', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
     { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
-]
+];
 // Task 2: Create a Function to Display Employee Shift Details
-function displayEmployeeShifts(employeeName) {
-    let employee = employees.find(employee => employee.name === employeeName) // Finding the employee in the array.
-    if (!employee) {
-        return `${employeeName} is not in database.`} // Incase a non-employee gets entered
-        console.log(`${employeeName} has the following shifts`)
-    return employee.shifts.map(shift =>{
-        return `${shift.hours} hours on ${shift.day}` // This displays the shifts given.
-        });
-    
-}
-console.log(displayEmployeeShifts("John"));
 
+function displayEmployeeShifts(employeeName) {
+    let employee = employees.find(employee => employee.name === employeeName);
+    if (employee) {
+        console.log(`${employee.name} has the following shifts`);
+        for (let shift of employee.shifts) {
+            console.log(`Day(s): ${shift.day}, Hours: ${shift.hours}`); 
+        }
+    }   else {console.log("ERROR: Employee not in database.");}
+};
+displayEmployeeShifts("Sara");
